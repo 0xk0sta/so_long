0c765406becc90b8ea5c6e1fabd2c800a987e223
acostal- <acostalcardenas@gmail.com>
2021-10-28 11:30:22 +0200

    cleanup

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -1,18 +1,5 @@
 #include "../includes/so_long.h"
 
-void	print_map(t_gvars *vars)
-{
-	int	i;
-
-	i = -1;
-	printf("H: %zu\n", vars->height);
-	while (++i < (int)vars->height - 1)
-		printf("%s  %d\t", vars->buffer[i], i);
-	printf("%s\n", vars->buffer[i]);
-	ft_free_map(vars);
-	exit((int)write(1, "MAP PRINTED SUCCESSFULLY\n", 25));
-}
-
 int	ft_error(t_gvars *vars, int etype)
 {
 	if (etype == 1)
